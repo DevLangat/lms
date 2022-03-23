266386699257ed4cd83daf804008daa45c4e98ee
Devlangat <kevokipa4@gmail.com>
2022-03-23 19:03:02 +0300

    loans

--- app/Http/Controllers/LoanApplicationController.php
+++ app/Http/Controllers/LoanApplicationController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
 class LoanApplicationController extends Controller
 {
     /**
@@ -19,7 +21,7 @@ class LoanApplicationController extends Controller
     public function index()
     {
         $loantypes = LoanType::all();
-        return view('members.loanapplication',compact('loantypes'));
+        return view('members.loanapplication', compact('loantypes'));
     }
 
     /**
@@ -29,7 +31,6 @@ class LoanApplicationController extends Controller
      */
     public function create()
     {
-        
     }
 
     /**
@@ -39,34 +40,43 @@ class LoanApplicationController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {   
-        $userid=$request->IDNo;
+    {
+        $userid = $request->IDNo;
         // if (LoanApplication::where('IDNo', '=', $request->IdNumber)->exists()) {
         // }
-        $count = LoanApplication::select('count(*)')->where('IDNo', $userid)
-               -> where('LoanCode', '=', $request->LoanCode)
-               ->get();  
-        if($count)
-    
-       $loan= new LoanApplication;
-       $loan->IDNo=$request->IDNo;
-       $loan->Loanno='1234';      
-       $loan->LoanCode=$request->LoanCode;
-       $loan->AmountApplied=$request->AmountApplied;
-       $loan->ApplicationDate=$request->ApplicationDate;
-       $loan->EffectDate= Carbon::now()->format('Y-m-d');
-       $loan->RecoverInterestFirst=true;
-       $loan->IntRate=$request->IntRate;
-       $loan->Rperiod=$request->Rperiod;
-       $loan->Createdby='User';
-       $loan->Approved=false;
-       $loan->ApprovedAmount='0';
-       $loan->RepayAmount='0';
-       $loan->IsDisbursed=false; 
-       $loan->ApprovedBy=$request->ApprovedBy; 
-       $loan->Modifiedby=$request->Modifiedby; 
-       $loan->ApprovedOn=$request->ApprovedOn; 
-        $loan->save();      
+        $count =  DB::table('loan_applications')
+            ->select(DB::raw('count(*) as count'))
+            ->where('IDNo', $userid)
+            ->where('LoanCode', '=', $request->LoanCode)
+            ->get();
+        if ($count) {
+            foreach($count as $loan_count){               
+                    $newcount=$loan_count->count+1;
+                    $loan_number=$request->LoanCode.$userid.'-'.$newcount;
+                    Log::info($loan_number);
+                
+            }
+        }
+
+           $loan= new LoanApplication;
+           $loan->IDNo=$request->IDNo;
+           $loan->Loanno=$loan_number;      
+           $loan->LoanCode=$request->LoanCode;
+           $loan->AmountApplied=$request->AmountApplied;
+           $loan->ApplicationDate=$request->ApplicationDate;
+           $loan->EffectDate= Carbon::now()->format('Y-m-d');
+           $loan->RecoverInterestFirst=true;
+           $loan->IntRate=$request->IntRate;
+           $loan->Rperiod=$request->Rperiod;
+           $loan->Createdby='User';
+           $loan->Approved=false;
+           $loan->ApprovedAmount='0';
+           $loan->RepayAmount='0';
+           $loan->IsDisbursed=false; 
+           $loan->ApprovedBy=$request->ApprovedBy; 
+           $loan->Modifiedby=$request->Modifiedby; 
+           $loan->ApprovedOn=$request->ApprovedOn; 
+            $loan->save();      
 
 
 
@@ -82,47 +92,42 @@ class LoanApplicationController extends Controller
      */
     public function show($id)
     {
-        
     }
- 
-    public function getUserbyid(Request $request){
- 
+
+    public function getUserbyid(Request $request)
+    {
+
         $userid = $request->userid;
-   
+
         $members = Member::select('*')->where('IdNumber', $userid)->get();
-        if($members){
-             // Fetch all records
-        foreach ($members as $member)
-        Log::info($member->Name);
-        return response()->json([
-            'member'=> $member
-        ]);
+        if ($members) {
+            // Fetch all records
+            foreach ($members as $member)
+                Log::info($member->Name);
+            return response()->json([
+                'member' => $member
+            ]);
+        } else {
+            Alert::error('No Member', 'The Member with ID No.' . strtoupper($request->userid) . ' ' . ' is not found');
         }
-        else{
-            Alert::error('No Member','The Member with ID No.'.strtoupper($request->userid).' '.' is not found');
-            
-        }
-       
-     }
-    public function getLoantypes(Request $request){
- 
+    }
+    public function getLoantypes(Request $request)
+    {
+
         $loancode = $request->loancode;
-   
+
         $loantypes = LoanType::select('*')->where('LoanCode', $loancode)->get();
-        if($loantypes){
-             // Fetch all records
-        foreach ($loantypes as $loantype)
-        Log::info($loantype->Ratio);
-        return response()->json([
-            'loantype'=> $loantype
-        ]);
+        if ($loantypes) {
+            // Fetch all records
+            foreach ($loantypes as $loantype)
+                Log::info($loantype->Ratio);
+            return response()->json([
+                'loantype' => $loantype
+            ]);
+        } else {
+            Alert::error('No Member', 'The Member with ID No.' . strtoupper($request->userid) . ' ' . ' is not found');
         }
-        else{
-            Alert::error('No Member','The Member with ID No.'.strtoupper($request->userid).' '.' is not found');
-            
-        }
-       
-     }
+    }
     /**
      * Show the form for editing the specified resource.
      *
@@ -137,12 +142,10 @@ class LoanApplicationController extends Controller
     {
         Log::info($request->ID);
         if (Member::where('IdNumber', '=', $request->ID)->exists()) {
-         
-            $member=Member::all();
+
+            $member = Member::all();
             return $member;
-           
         }
-        
     }
 
     /**
